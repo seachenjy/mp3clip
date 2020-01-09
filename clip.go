05fd84a0accf307443c9d10321336cabe69b7e4c
seachen <364095436@qq.com>
2020-01-09 13:06:08 +0800

    add file

---
+++ clip.go
@@ -0,0 +1,13 @@
+package mp3clip
+
+import "os"
+
+//Cliper cliper
+type Cliper struct {
+	File *os.File
+}
+
+//Decode read mp3 file get the base info
+func Decode(f *os.File) {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module mp3clip
+
+go 1.13
